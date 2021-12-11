a3bc3a399210b745d32fd33cfedc8795868f545c
Dario Vogogna <stein.dario@gmail.com>
2021-12-11 16:16:51 +0100

    Day 1 part 2 completed

--- src/day01/day01.go
+++ src/day01/day01.go
@@ -7,6 +7,15 @@ import (
   "strconv"
 )
 
+func contains(s []int, e int) bool {
+  for _, a := range s {
+    if a == e {
+      return true
+    }
+  }
+  return false
+}
+
 func check(e error) {
   if e != nil {
     panic(e)
@@ -18,16 +27,36 @@ func main() {
   check(err)
 
   scanner := bufio.NewScanner(f)
-  var count int = -1
-  var prevValue int
-  for scanner.Scan() {
-    var line string = scanner.Text()
-    currentValue, err := strconv.Atoi(line)
-    if err == nil && prevValue != -1 && prevValue < currentValue {
-      count++
-    }
+  var count int
+  prevValue := -1
+  currentWindow := []int{-1, -1, -1}
 
-    prevValue = currentValue
+  for scanner.Scan() {
+    line := scanner.Text()
+    currentValue, err := strconv.Atoi(line)
+    check(err)
+
+    // Remove first array element
+    currentWindow = currentWindow[1:]
+    // Insert the newly read one as last element
+    currentWindow = append(currentWindow, currentValue)
+    if !contains(currentWindow, -1) {
+      acc := 0
+      for i := 0; i < len(currentWindow); i++ {
+        acc += currentWindow[i]
+      }
+
+      fmt.Printf("Current window sum: %d", acc)
+
+      if prevValue != -1 && acc > prevValue {
+        fmt.Printf(" Increased\n")
+        count++
+      } else {
+        fmt.Printf("\n")
+      }
+
+      prevValue = acc
+    }
   }
 
   fmt.Printf("Total increases: %d\n", count)
